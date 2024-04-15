713bdc15937aa307eb58f73422b33a6fc340a0c6
sharapov2701 <sharapov2701@gmail.com>
2024-04-15 04:15:43 +0300

    add linters

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: ['@nuxt/eslint-config', 'plugin:prettier/recommended'],
+  plugins: ['unused-imports'],
+  rules: {
+    'unused-imports/no-unused-imports-ts': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/v-on-event-hyphenation': [
+      'error',
+      'always',
+      {
+        autofix: true
+      }
+    ],
+    'vue/require-default-prop': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto'
+}
