dea3c93ba445656ed461cd6116ba9579ebf94cf7
sharapov2701 <sharapov2701@gmail.com>
2024-04-25 16:24:49 +0300

    setup base styles and font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300]
+          }
+        }
+      }
+    ]
+  ],
   shadcn: {
     prefix: 'Ui',
     componentDir: './components/ui'
