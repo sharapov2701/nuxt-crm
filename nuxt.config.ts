958e26ad1c4d30d04b427a8446e7a4619b42116b
sharapov2701 <sharapov2701@gmail.com>
2024-04-25 16:09:58 +0300

    add button component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   shadcn: {
-    prefix: '',
+    prefix: 'Ui',
     componentDir: './components/ui'
   }
 })
