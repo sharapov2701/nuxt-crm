d4070e9c6b6b4ae882c80c4248851c0d25efa8f7
sharapov2701 <sharapov2701@gmail.com>
2024-04-25 15:13:44 +0300

    install shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss']
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
 })
