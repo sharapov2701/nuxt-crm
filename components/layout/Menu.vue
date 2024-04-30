35db4410b01e409d195973fbb1ee639c1934a2e4
sharapov2701 <sharapov2701@gmail.com>
2024-04-30 20:39:35 +0300

    add menu

---
+++ components/layout/Menu.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { MENU_DATA } from './menu.data'
+</script>
+
+<template>
+  <div>
+    <nuxt-link
+      v-for="item in MENU_DATA"
+      :key="item.name"
+      class="flex items-center py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover:shadow transition-all mb-2.5"
+      :to="item.url"
+    >
+      <icon :name="item.icon" class="mr-3" />
+      <span>{{ item.name }}</span>
+    </nuxt-link>
+  </div>
+</template>
--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -2,12 +2,14 @@
 
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
-    <nuxt-link class="mb-10 block" to="/">
-      <nuxt-img src="/logo.svg" width="100px" class="mx-auto" />
+    <nuxt-link to="/" class="mb-10 block">
+      <nuxt-img src="/logo.svg" alt="" width="140px" class="mx-auto" />
     </nuxt-link>
 
     <button class="absolute top-2 right-3 transition-colors hover:text-primary">
-      <icon name="line-md:logout" size="22" />
+      <icon name="line-md:logout" size="20" />
     </button>
+
+    <layout-menu />
   </aside>
 </template>
