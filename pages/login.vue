01bbc1d4dec2377e4e4b2e6ce8512b2279c3a480
sharapov2701 <sharapov2701@gmail.com>
2024-05-05 14:47:05 +0300

    update login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
-useSeoMeta({ title: 'Login | Nuxt CRM' })
+import { v4 as uuid } from 'uuid'
+
+useSeoMeta({
+  title: 'Login | Nuxt CRM'
+})
+
+const router = useRouter()
+const authStore = useAuthStore()
+const isLoadingStore = useIsLoadingStore()
 
 const name = ref('')
 const email = ref('')
 const password = ref('')
+
+const login = async () => {
+  isLoadingStore.set(true)
+  await account.createEmailSession(email.value, password.value)
+  const response = await account.get()
+  if (response) {
+    authStore.set({
+      email: response.email,
+      name: response.name,
+      status: response.status
+    })
+  }
+
+  email.value = ''
+  password.value = ''
+  name.value = ''
+
+  await router.push('/')
+  isLoadingStore.set(false)
+}
+
+const register = async () => {
+  await account.create(uuid(), email.value, password.value, name.value)
+  await login()
+}
 </script>
 
 <template>
@@ -14,13 +47,13 @@ const password = ref('')
       <form>
         <ui-input v-model="email" placeholder="Email" type="email" class="mb-3" />
         <ui-input v-model="password" placeholder="Password" type="password" class="mb-3" />
-        <ui-input v-model="name" placeholder="Name" type="text" class="mb-3" />
-      </form>
+        <ui-input v-model="name" placeholder="Name" type="name" class="mb-3" />
 
-      <div class="flex items-center justify-center gap-5">
-        <ui-button type="button">Sign in</ui-button>
-        <ui-button type="button">Sign up</ui-button>
-      </div>
+        <div class="flex items-center justify-center gap-5">
+          <ui-button type="button" @click="login">Sign in</ui-button>
+          <ui-button type="button" @click="register">Sign up</ui-button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
