b4bcc9cf68c4f8d67217ccaea7236a67d9d3cfa3
sharapov2701 <sharapov2701@gmail.com>
2024-05-05 15:08:04 +0300

    implement logout

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { useAuthStore, useIsLoadingStore } from '~/store/auth.store'
+
+const router = useRouter()
+const store = useAuthStore()
+const isLoadingStore = useIsLoadingStore()
+
+const logout = async () => {
+  isLoadingStore.set(true)
+  await account.deleteSession('current')
+  store.clear()
+  await router.push('/login')
+  isLoadingStore.set(false)
+}
+</script>
 
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
@@ -6,7 +20,7 @@
       <nuxt-img src="/logo.svg" alt="" width="140px" class="mx-auto" />
     </nuxt-link>
 
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary" @click="logout">
       <icon name="line-md:logout" size="20" />
     </button>
 
