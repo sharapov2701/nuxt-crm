a31fc69813b5645776d9549e9f91bad8c2d67001
sharapov2701 <sharapov2701@gmail.com>
2024-05-05 15:10:00 +0300

    add loader

---
+++ components/layout/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <nuxt-img src="/loader.svg" alt="" width="220" />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  @apply bg-sidebar flex items-center justify-center w-screen h-screen;
+}
+</style>
