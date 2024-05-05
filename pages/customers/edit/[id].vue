750127c462aaa1809ecf8bf432083cdd873313b1
sharapov2701 <sharapov2701@gmail.com>
2024-05-05 15:55:57 +0300

    add customer page

---
+++ pages/customers/edit/[id].vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { v4 as uuid } from 'uuid'
+import { useMutation, useQuery } from '@tanstack/vue-query'
+
+import type { ICustomer } from '~/types/deals.types'
+import { COLLECTION_CUSTOMERS, DB_ID, STORAGE_ID } from '~/app.constants'
+
+interface InputFileEvent extends Event {
+  target: HTMLInputElement
+}
+
+interface ICustomerFormState extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {}
+
+useSeoMeta({
+  title: `Редактирование компании`
+})
+
+const route = useRoute()
+const customerId = route.params.id as string
+
+const { handleSubmit, defineField, setFieldValue, setValues, values } = useForm<ICustomerFormState>()
+
+const { data, isSuccess } = useQuery({
+  queryKey: ['get customer', customerId],
+  queryFn: () => DB.getDocument(DB_ID, COLLECTION_CUSTOMERS, customerId)
+})
+
+watch(isSuccess, () => {
+  const initialData = data.value as unknown as ICustomerFormState
+  setValues({
+    email: initialData.email,
+    avatar_url: initialData.avatar_url,
+    from_source: initialData.from_source || '',
+    name: initialData.name
+  })
+})
+
+const [name, nameAttrs] = defineField('name')
+const [email, emailAttrs] = defineField('email')
+const [fromSource, fromSourceAttrs] = defineField('from_source')
+
+const { mutate, isPending } = useMutation({
+  mutationKey: ['update customer', customerId],
+  mutationFn: (data: ICustomerFormState) => DB.updateDocument(DB_ID, COLLECTION_CUSTOMERS, customerId, data)
+})
+
+const { mutate: uploadImage, isPending: isUploadImagePending } = useMutation({
+  mutationKey: ['upload image'],
+  mutationFn: (file: File) => storage.createFile(STORAGE_ID, uuid(), file),
+  onSuccess(data) {
+    const response = storage.getFileDownload(STORAGE_ID, data.$id)
+    setFieldValue('avatar_url', response.href)
+  }
+})
+
+const onSubmit = handleSubmit(values => {
+  mutate(values)
+})
+</script>
+
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">Редактирование {{ (data as unknown as ICustomerFormState)?.name }}</h1>
+
+    <form class="form" @submit="onSubmit">
+      <ui-input v-model="name" v-bind="nameAttrs" class="input" type="text" placeholder="Наименование" />
+
+      <ui-input v-model="email" v-bind="emailAttrs" class="input" type="text" placeholder="Email" />
+      <ui-input v-model="fromSource" v-bind="fromSourceAttrs" class="input" type="text" placeholder="Откуда пришел?" />
+
+      <img
+        v-if="values.avatar_url || isUploadImagePending"
+        class="rounded-full my-4"
+        alt=""
+        width="50"
+        height="50"
+        :src="values.avatar_url"
+      />
+
+      <div class="grid w-full max-w-sm items-center gap-1.5 input">
+        <label>
+          <div class="text-sm mb-2">Логотип</div>
+
+          <ui-input
+            type="file"
+            :disabled="isUploadImagePending"
+            :onchange="(e: InputFileEvent) => e?.target?.files?.length && uploadImage(e.target.files[0])"
+          />
+        </label>
+      </div>
+
+      <ui-button class="mt-3" variant="secondary" :disabled="isPending">
+        {{ isPending ? 'Загрузка...' : 'Сохранить' }}
+      </ui-button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.input {
+  @apply border-[#161c26] mb-4 placeholder:text-[#748092] focus:border-border transition-colors;
+}
+</style>
---
+++ pages/customers/index.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { useQuery } from '@tanstack/vue-query'
+
+import type { ICustomer } from '~/types/deals.types'
+import { COLLECTION_CUSTOMERS, DB_ID } from '~/app.constants'
+
+useSeoMeta({
+  title: 'Customers | CRM System'
+})
+
+const { data: customers, isLoading } = useQuery({
+  queryKey: ['customers'],
+  queryFn: () => DB.listDocuments(DB_ID, COLLECTION_CUSTOMERS)
+})
+</script>
+
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">Наши клиенты</h1>
+
+    <div v-if="isLoading">Loading...</div>
+
+    <ui-table v-else>
+      <ui-table-header>
+        <ui-table-row>
+          <ui-table-head class="w-[200px]">Изображение</ui-table-head>
+          <ui-table-head class="w-[300px]">Наименование</ui-table-head>
+          <ui-table-head class="w-[200px]">Email</ui-table-head>
+          <ui-table-head>Откуда пришел</ui-table-head>
+        </ui-table-row>
+      </ui-table-header>
+
+      <ui-table-body>
+        <ui-table-row v-for="customer in customers?.documents as unknown as ICustomer[]" :key="customer.$id">
+          <ui-table-cell>
+            <nuxt-link :href="`/customers/edit/${customer.$id}`">
+              <nuxt-img :src="customer.avatar_url" :alt="customer.name" width="50" height="50" class="rounded-full" />
+            </nuxt-link>
+          </ui-table-cell>
+
+          <ui-table-cell class="font-medium">
+            {{ customer.name }}
+          </ui-table-cell>
+
+          <ui-table-cell>{{ customer.email }}</ui-table-cell>
+
+          <ui-table-cell>{{ customer.from_source }}</ui-table-cell>
+        </ui-table-row>
+      </ui-table-body>
+    </ui-table>
+  </div>
+</template>
